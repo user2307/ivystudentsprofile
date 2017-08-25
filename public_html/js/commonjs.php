1ae4a49daef4ed6fce7f14c481315d1d5cf5a0b0
Vandana Arora <user2307@users.noreply.github.com>
2017-08-25 15:00:43 -0400

    Add files via upload

---
+++ public_html/js/commonjs.php
@@ -0,0 +1,37 @@
+<?php
+echo '
+   <script type="text/javascript" src="/../js-plugin/respond/respond.min.js"></script>
+        <script type="text/javascript" src="/../bootstrap/js/bootstrap.js"></script>
+        <script type="text/javascript" src="/../js-plugin/easing/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="/../js-plugin/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/magnific-popup/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/isotope/jquery.isotope.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/neko-contact-ajax-plugin/js/jquery.form.js"></script>
+        <script type="text/javascript" src="/../js-plugin/neko-contact-ajax-plugin/js/jquery.validate.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/parallax/js/jquery.stellar.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/camera/camera.min.js"></script>
+        <script type="text/javascript" src="/../js-plugin/appear/jquery.appear.js"></script>
+        <script type="text/javascript" src="/js/custom.js"></script>
+<script type="text/javascript" src="/../search/search.js"></script>
+<script>
+$(window).scroll(function() {
+if( $(this).width()>750){
+if ($(this).scrollTop() > 1 && $(this).width()>1024 ){
+    $(\'div.input-group.input-group-sm\').attr(\'style\',\'padding:10px 11px\');
+  }
+  else{
+  $(\'div.input-group.input-group-sm\').attr(\'style\',\'padding:30px 11px\');
+}
+}
+else
+ $(\'div.input-group.input-group-sm\').attr(\'style\',\'padding:0 0\');
+
+
+
+});
+</script>
+
+
+';
+
+?>
